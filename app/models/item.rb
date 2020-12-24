a4154fc61d7bd7c5e6b55a3974aa4ba3ffd33426
CatAisa <suzuki03331@icloud.com>
2020-12-24 21:48:12 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -8,14 +9,18 @@ class Item < ApplicationRecord
   belongs_to :period
 
   with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
     validates :category_id
     validates :condition_id
     validates :payment_id
     validates :prefecture_id
     validates :period_id
+    validates :price
   end
   
-  with_options numerilality: {other_than: 1} do
+  with_options numericality: {other_than: 0} do
     validates :category_id
     validates :condition_id
     validates :payment_id
