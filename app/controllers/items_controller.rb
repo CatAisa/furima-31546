33dcc3e1e342877f5cbfa60f7d27ff0562d5a3f7
CatAisa <suzuki03331@icloud.com>
2020-12-24 21:47:49 +0900

    商品投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,12 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create(item_params)
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :payment_id, :prefecture_id, :period_id, :price).merge(user_id: current_user.id)
+  end
+
 end
