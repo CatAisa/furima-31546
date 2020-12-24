08356cbc965c62550b1ac34c67d5c562770e6916
CatAisa <suzuki03331@icloud.com>
2020-12-24 20:54:44 +0900

    ActiveHashに関するバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,26 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :payment
+  belongs_to :prefecture
+  belongs_to :period
+
+  with_options presence: true do
+    validates :category_id
+    validates :condition_id
+    validates :payment_id
+    validates :prefecture_id
+    validates :period_id
+  end
+  
+  with_options numerilality: {other_than: 1} do
+    validates :category_id
+    validates :condition_id
+    validates :payment_id
+    validates :prefecture_id
+    validates :period_id
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  
   with_options presence: true do
     validates :nickname
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'is invalid. Input full-width characters.' }
@@ -15,5 +17,4 @@ class User < ApplicationRecord
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Input include both letters and numbers' }
 
-  has_many :items
 end
