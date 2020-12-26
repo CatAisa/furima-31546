58468206bd9e8a8edfd5d3d56cafa1b3ba773224
CatAisa <suzuki03331@icloud.com>
2020-12-26 14:25:10 +0900

    テストコードの実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Name.last_name}
+    description {Faker::Lorem.sentence}
+    category_id {Faker::Number.non_zero_digit}
+    condition_id {Faker::Number.non_zero_digit}
+    payment_id {Faker::Number.non_zero_digit}
+    prefecture_id {Faker::Number.between(from: 1, to: 47)}
+    period_id {Faker::Number.non_zero_digit}
+    price {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
   end
 end
