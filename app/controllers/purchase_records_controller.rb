f857365eda899daebe95406bafdb88f9f223a4c8
CatAisa <suzuki03331@icloud.com>
2020-12-28 08:01:48 +0900

    purchase_recordがデータ保存されるように仮設定

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,5 +1,17 @@
 class PurchaseRecordsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase_record = PurchaseRecord.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_record = @item.purchase_records.new(purchase_record_params)
+    @purchase_record.save
+  end
+
+  private
+  def purchase_record_params
+    params.require(:purchase_record).permit().merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
