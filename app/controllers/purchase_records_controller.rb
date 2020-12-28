8194ac2829deea9101321868757d9f70a68bcc61
CatAisa <suzuki03331@icloud.com>
2020-12-28 23:27:28 +0900

    Formオブジェクトパターンでの実装に変更

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,20 +1,18 @@
 class PurchaseRecordsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @purchase = PurchaseRecord.new
+    @purchase_address = PurchaseAddress.new
   end
 
   def create
-    purchase = PurchaseRecord.create(purchase_params)
-    Address.create(address_params(purchase))
+    user = current_user
+    item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
+    @purchase_address.save(user, item)
   end
 
   private
-  def purchase_params
-    params.permit().merge(user_id: current_user.id, item_id: params[:item_id])
-  end
-
-  def address_params(purchase)
-    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(purchase_record_id: purchase.id)
+  def purchase_address_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
   end
 end
