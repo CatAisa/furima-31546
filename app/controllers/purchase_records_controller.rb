95aacf54ec1ea3af9ba6893bcb6e562dfb626feb
CatAisa <suzuki03331@icloud.com>
2020-12-28 13:26:43 +0900

    address保存の叩き台

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,15 +1,21 @@
 class PurchaseRecordsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @purchase_record = PurchaseRecord.new
+    @purchase = PurchaseRecord.new
   end
 
   def create
-    @purchase_record = PurchaseRecord.create(purchase_record_params)
+    purchase = PurchaseRecord.new(purchase_params)
+    address = Address.new(address_params(purchase))
+    binding.pry
   end
 
   private
-  def purchase_record_params
-    params.require(:purchase_record).permit().merge(user_id: current_user.id, item_id: params[:item_id])
+  def purchase_params
+    params.permit().merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def address_params(purchase)
+    params.permit().merge(purchase_record_id: purchase.id)
   end
 end
