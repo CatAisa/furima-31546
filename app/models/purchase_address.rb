37808ed5d4fbeda93ad37e50f9633bebab009d4e
CatAisa <suzuki03331@icloud.com>
2020-12-29 11:43:49 +0900

    空だと保存されないバリデーションを設定

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,6 +2,14 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
 
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number
+  end
+
   def save(user, item)
     purchase = PurchaseRecord.create(user_id: user.id, item_id: item.id)
     address = Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_record_id: purchase.id)
