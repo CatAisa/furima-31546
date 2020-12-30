0bc816916a2755eef7a1766d649a76e6b2337c29
CatAisa <suzuki03331@icloud.com>
2020-12-30 22:07:34 +0900

    テストコードの実施

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :purchase_address do
+    postal_code {'123-4567'}
+    prefecture_id {Faker::Number.between(from: 1, to: 47)}
+    city {Faker::Address.city}
+    house_number {Faker::Address.street_address}
+    building_name {Faker::Address.street_address}
+    phone_number {Faker::Number.leading_zero_number(digits: 11)}
+    token {"tok_abcdefghijklmn0123456789"}
+  end
+end
