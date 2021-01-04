9555c912ea492896ff4a3d68638dc1420fb0ade7
CatAisa <suzuki03331@icloud.com>
2021-01-04 15:26:46 +0900

    不必要な記述、ファイルの削除

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -13,7 +13,7 @@ class PurchaseAddress
 
   def save(user, item)
     purchase = PurchaseRecord.create(user_id: user.id, item_id: item.id)
-    address = Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number,
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number,
                              building_name: building_name, phone_number: phone_number, purchase_record_id: purchase.id)
   end
 end
