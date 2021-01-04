7ede14c6c3c6c3d988ca97aa6a44b5d69e83d67d
CatAisa <suzuki03331@icloud.com>
2021-01-04 17:06:25 +0900

    user_idとitem_idのバリデーションを追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Input correctly.' }
@@ -8,13 +8,15 @@ class PurchaseAddress
     validates :city
     validates :house_number
     validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input half-width numbers.' }
-    validates :phone_number, length: {maximum: 11, message: 'is invalid. Input number within 11 digits.'}
     validates :token
+    validates :user_id
+    validates :item_id
   end
+  validates :phone_number, length: { maximum: 11, message: 'is invalid. Input number within 11 digits.' }
 
-  def save(user, item)
-    purchase = PurchaseRecord.create(user_id: user.id, item_id: item.id)
+  def save
+    purchase = PurchaseRecord.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number,
-                             building_name: building_name, phone_number: phone_number, purchase_record_id: purchase.id)
+                   building_name: building_name, phone_number: phone_number, purchase_record_id: purchase.id)
   end
 end
