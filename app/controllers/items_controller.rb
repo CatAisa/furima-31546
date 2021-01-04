ff4e7088798aceae3682cadfeb6883394dbc771c
CatAisa <suzuki03331@icloud.com>
2021-01-04 11:48:24 +0900

    Item.findと出品者以外リダイレクトを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_find, only: [:show, :edit, :update]
+  before_action :user_judge, only: :edit
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,16 +21,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     redirect_to root_path if @item.user.id != current_user.id
   end
 
   def update
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -41,5 +43,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :payment_id, :prefecture_id,
                                  :period_id, :price).merge(user_id: current_user.id)
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
+  end
+
+  def user_judge
+    redirect_to root_path if @item.user.id != current_user.id
   end
 end
